46f25d3376ee62c9390f886d8674f93921b17a56
IgnacioBarraza <ignacio.barraza.rioja@gmail.com>
2022-11-13 13:17:00 -0300

    Edit success on category route:)

--- src/controllers/category.controlle.js
+++ src/controllers/category.controlle.js
@@ -15,9 +15,10 @@ export const getCategory = async (req, res) => {
     try {
         const [rows] = await pool.query('SELECT * FROM product WHERE category = ?', [req.params.category])
 
-        if (rows.lenght <= 0) return res.status(404).json({
+        if (rows.length <= 0) return res.status(404).json({
             message: 'Categoria no encontrada'
         })
+    res.json(rows)
     } catch (error) {
         return res.status(500).json({
             message: 'Algo salio mal'
